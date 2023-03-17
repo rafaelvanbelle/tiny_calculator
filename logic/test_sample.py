97cf7efbea7ee3be417fffe49039077cec055ae1
Rafael Van Belle <rafke.vb@gmail.com>
2023-03-17 20:04:09 +0100

    commit to first branch

--- logic/test_sample.py
+++ logic/test_sample.py
@@ -1,5 +1,9 @@
 from logic import add_numbers
 
 
-def test_answer():
-    assert add_numbers(3, 4) == 7
+class TestAnswers:
+    def test_answer(self):
+        assert add_numbers(3, 4) == 7
+
+    def test_two(self):
+        assert add_numbers(-1, -2) == -3
