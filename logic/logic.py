010b841b07164f203d6a7de69599ff2d7494f29e
Rafael Van Belle <rafke.vb@gmail.com>
2023-03-17 19:59:38 +0100

    second commit

---
+++ logic/logic.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return a + b
+
+
+def sub_numbers(a, b):
+    return a - b
+
+
+
---
+++ logic/test_sample.py
@@ -0,0 +1,5 @@
+from logic import add_numbers
+
+
+def test_answer():
+    assert add_numbers(3, 4) == 7
